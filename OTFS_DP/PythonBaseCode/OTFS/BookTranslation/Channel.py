247a07e3aaa870afcb2d082a0e2d207ba79f7685
Tonix22 <emiliotonix@gmail.com>
2022-10-01 13:54:36 -0600

    Channel Changes

--- OTFS_DP/PythonBaseCode/OTFS/BookTranslation/Channel.py
+++ OTFS_DP/PythonBaseCode/OTFS/BookTranslation/Channel.py
@@ -4,8 +4,19 @@ from GPP3 import *
 from math import cos,pi,sqrt
 from random import randint
 
+#delay_resolution
+#k_max
+
 class Channel():
-    def __init__(self,delay_resolution,k_max, GPP_standard = "EVA" , Spectrum=SprectrumType.JAKES):
+    def __init__(self,Transmiter, GPP_standard = "EVA" , Spectrum=SprectrumType.JAKES):
+        
+        self.TX   = Transmiter
+        self.taps = None
+        self.gs   = None # Tapped Channel
+        self.G    = None # Time channel matrix
+        self.H_tilda   = None    # Delay-time channel matrix
+        self.H    = None # Delay-Doppler
+        self.grid_size = self.Tx.N*self.Tx.M
         
         # assuming Jakes spectrum
         if(Spectrum == SprectrumType.STANDARD):
@@ -14,9 +25,11 @@ class Channel():
             # generate channel coefficients (Rayleigh fading)
             self.g_i = np.sqrt(Model.pdp_linear)@(np.sqrt(2)*(np.random.randn((1,Model.taps))+1j*np.random.randn((1,Model.taps))))
             # generate delay taps (assuming integer delay taps)
-            self.l_i=np.around(Model.delay/delay_resolution)
+            self.l_i=np.around(Model.delay/self.TX.delay_resolution)
             # Generate Doppler taps (assuming Jakes spectrum)
-            self.k_i = (k_max*cos(2*pi*np.random.randn((1,Model.taps))))
+            self.k_i = (self.TX.k_max*cos(2*pi*np.random.randn((1,Model.taps))))
+            
+            self.taps = Model.taps
 
         # assuming uniform spectrum
         if(Spectrum == SprectrumType.SYNTHETIC):
@@ -31,11 +44,36 @@ class Channel():
             self.l_i = np.random.randint(0,high = l_max, size = (1,taps))
             self.l_i = self.l_i - np.min(self.l_i)
             #generate Doppler taps (assuming uniform spectrum) [-k_max,k_max])
-            self.k_i = k_max-2*k_max*np.random.randn((1,taps))
+            self.k_i  = k_max-2*k_max*np.random.randn((1,taps))
+            self.taps = taps
             
+        self.delay_spread = max(self.l_i)
+        
+        
+    def Gen_Tapped_delay_line(self):
         # Generate discrete delay-time channel coefficients and matrix.
+        #S[q] tap shifting
+        z = np.exp(1j*2*pi/(self.grid_size))
+        
+        #Generate discrete-time baseband channel in TDL form (Eq. (2.22))
+        #DISCRETE TIME
+        self.gs = np.zeros((self.delay_spread+1,self.grid_size))
+        for q in range (0,self.grid_size):
+            for i in range(0,self.taps):
+                shift = z**(self.k_i[i]*(q-self.l_i[i]))
+                self.gs[self.l_i,q]= self.gs[self.l_i,q]+self.g_i[i]*(shift)
+        
+    def Gen_discrete_time(self):
+        self.G = np.zeros(self.grid_size,self.grid_size)
+        for q in range(0,self.grid_size):
+            for ell in range(0,self.delay_spread):
+                if q >ell:
+                    self.G[q,q-ell] = self.gs[ell,q]
+        
+        
 
-        # Generate delay-time and delay-Doppler channel matrix.
+    def Gen_delay_time(self):
+        self.H_tilda=P*G*P.T
         
         # Generate r by passing the Tx signal through the channel.
 
