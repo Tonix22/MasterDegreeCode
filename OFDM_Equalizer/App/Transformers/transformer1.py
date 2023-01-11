fb0612007f3b0f22755e207f23dbe7dc693083b5
tonix22 <emiliotonix@gmail.com>
2023-01-10 20:01:28 -0600

    transfomer update

--- OFDM_Equalizer/App/Transformers/transformer1.py
+++ OFDM_Equalizer/App/Transformers/transformer1.py
@@ -188,6 +188,86 @@ def batchify_data(data, batch_size=16, padding=False, padding_token=-1):
 
     return batches
 
+def train_loop(model, opt, loss_fn, dataloader):
+    model.train()
+    total_loss = 0
+    
+    for batch in dataloader:
+        X, y = batch[:, 0], batch[:, 1]
+        X, y = torch.tensor(X).to(device), torch.tensor(y).to(device)
+
+        # Now we shift the tgt by one so with the <SOS> we predict the token at pos 1
+        y_input = y[:,:-1]
+        y_expected = y[:,1:]
+        
+        # Get mask to mask out the next words
+        sequence_length = y_input.size(1)
+        tgt_mask = model.get_tgt_mask(sequence_length).to(device)
+
+        # Standard training except we pass in y_input and tgt_mask
+        pred = model(X, y_input, tgt_mask)
+
+        # Permute pred to have batch size first again
+        # move dimensions
+        pred = pred.permute(1, 2, 0)      
+        loss = loss_fn(pred, y_expected)
+
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+    
+        total_loss += loss.detach().item()
+        
+    return total_loss / len(dataloader)
+
+def validation_loop(model, loss_fn, dataloader):
+    model.eval()
+    total_loss = 0
+    
+    with torch.no_grad():
+        for batch in dataloader:
+            X, y = batch[:, 0], batch[:, 1]
+            X, y = torch.tensor(X, dtype=torch.long, device=device), torch.tensor(y, dtype=torch.long, device=device)
+
+            # Now we shift the tgt by one so with the <SOS> we predict the token at pos 1
+            y_input = y[:,:-1]
+            y_expected = y[:,1:]
+            
+            # Get mask to mask out the next words
+            sequence_length = y_input.size(1)
+            tgt_mask = model.get_tgt_mask(sequence_length).to(device)
+
+            # Standard training except we pass in y_input and src_mask
+            pred = model(X, y_input, tgt_mask)
+
+            # Permute pred to have batch size first again
+            pred = pred.permute(1, 2, 0)      
+            loss = loss_fn(pred, y_expected)
+            total_loss += loss.detach().item()
+        
+    return total_loss / len(dataloader) # average loss
+
+def fit(model, opt, loss_fn, train_dataloader, val_dataloader, epochs):
+    # Used for plotting later on
+    train_loss_list, validation_loss_list = [], []
+    
+    print("Training and validating model")
+    for epoch in range(epochs):
+        print("-"*25, f"Epoch {epoch + 1}","-"*25)
+        
+        train_loss = train_loop(model, opt, loss_fn, train_dataloader)
+        train_loss_list += [train_loss]
+        
+        validation_loss = validation_loop(model, loss_fn, val_dataloader)
+        validation_loss_list += [validation_loss]
+        
+        print(f"Training loss: {train_loss:.4f}")
+        print(f"Validation loss: {validation_loss:.4f}")
+        print()
+        
+    return train_loss_list, validation_loss_list
+    
+
 
 train_data = generate_random_data(9000)
 val_data   = generate_random_data(3000)
@@ -199,3 +279,5 @@ device  = "cuda" if torch.cuda.is_available() else "cpu"
 model   = Transformer(num_tokens=4, dim_model=8, num_heads=2, num_encoder_layers=3, num_decoder_layers=3, dropout_p=0.1).to(device)
 opt     = torch.optim.SGD(model.parameters(), lr=0.01)
 loss_fn = nn.CrossEntropyLoss()
+
+train_loss_list, validation_loss_list = fit(model, opt, loss_fn, train_dataloader, val_dataloader, 10)
