c4fd3a3334749f1328d37edb04cc7adbeac806de
tonix22 <emiliotonix@gmail.com>
2023-01-12 17:27:20 -0600

    For new Angle cost function

--- OFDM_Equalizer/conf/config.py
+++ OFDM_Equalizer/conf/config.py
@@ -17,7 +17,7 @@ pth1         = base + "QPSK/Real/OFDM_Eq_SNR_(45_25)_(REAL)_-12_1_2023-11_28.pth
 pth2         = base + "QPSK/Imag/OFDM_Eq_SNR_(45_25)_(IMAG)_-12_1_2023-11_28.pth"
 pth_QAM      = base + "Constelation-26_10_2022-16_49.pth"
 #polar strategy
-pth_angle  = base + "QPSK/Phase/OFDM_Eq_SNR_(45_25)_(ANGLE)_-11_1_2023-17_17.pth"
+pth_angle  = base + "QPSK/Phase/OFDM_Eq_SNR_(45_15)_(ANGLE)_-12_1_2023-15_38.pth"
 pth_abs    = base + "OFDM_Eq_SNR_(40_20)_(ABS)_-9_12_2022-14_5.pth"
 #binary Cross entropy
 pth_bce    = base + "OFDM_Eq_SNR_(40_20)_(FOUR)_-9_12_2022-11_35.pth"
@@ -31,7 +31,7 @@ folder_training_model = "QPSK"
 BEST_SNR   = 45
 WORST_SNR  = 15
 STEP_SNR   = -5
-EPOCHS     = 4
+EPOCHS     = 2
 LEARNING_RATE = .001
 EPSILON = .01
 #for Phase, LR=.0007, EPISLON = .007
