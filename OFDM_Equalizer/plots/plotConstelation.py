0d847a004db89be75549b2a924616407728bdcb0
Tonix22 <emiliotonix@gmail.com>
2023-02-08 00:16:48 -0600

    Plot constelation refined

--- OFDM_Equalizer/plots/plotConstelation.py
+++ OFDM_Equalizer/plots/plotConstelation.py
@@ -11,28 +11,43 @@ sys.path.insert(0, main_path+"controllers")
 from Recieved import RX
 
 
-QAM = 16
+
+# ----------- Data Setup  ---------
+QAM         = 16
 realization =  6063 # the argmax given out study in the dataset
+#BINS analysis
+step = 1/4
 #max_index = np.unravel_index(np.argmax(rx.H), rx.H.shape)
+
+# ----------- Data Generation ---------
 #Get all samples
-rx = RX(QAM,"Unit_Pow","Complete")
+rx        = RX(QAM,"Unit_Pow","Complete")
 bits      = rx.Qsym.bits[:,realization].squeeze()
-#Data multiplied by channel
+
+# Data multiplied by channel
 data      = rx.Qsym.r[:,realization]
-#data multiplied by hermitian
+data      = rx.Qsym.r[:,realization]/np.max(np.abs(data))
+
+# Data multiplied by hermitian
 hermitian = np.array(np.matrix(rx.H[:,:,realization]).H@rx.Qsym.r[:,realization])
 hermitian = hermitian/np.max(np.abs(hermitian))
-#ground truth
+# Ground truth
 truth     = rx.Qsym.GroundTruth[:,realization]
 truth     = truth/np.max(np.abs(truth))
 
-cmap = cmx.jet
-c_norm = colors.Normalize(vmin=0, vmax=48)
+# ----------- Color Map to scatter ---------
+cmap       = cmx.jet
+c_norm     = colors.Normalize(vmin=0, vmax=48)
 scalar_map = cmx.ScalarMappable(norm=c_norm, cmap=cmap)
-colors = [scalar_map.to_rgba(i) for i in range(48)]
+colors     = [scalar_map.to_rgba(i) for i in range(48)]
 
+# ----------- Grid Creation ----------------
+# Define bins for the real and imaginary parts of the data
+binsx = np.arange(-1, 1 + step, step)
+binsy = np.arange(-1, 1 + step, step)
 
-def plot_scatter_values(ax,rx,vect):
+# ----------- Generic Plot function ----------
+def plot_scatter_values(ax,rx,vect,title):
     real = np.real(vect)
     imag = np.imag(vect)
     
@@ -42,13 +57,15 @@ def plot_scatter_values(ax,rx,vect):
         ax.text(real[i], imag[i],format(bits[i], '0{}b'.format(rx.bitsframe)),fontsize=10)
 
     ax.grid(True)
+    ax.set_xticks(binsx)
+    ax.set_yticks(binsy)
     #plot title
     ax.set_xlabel('Real Part')
     ax.set_ylabel('Imaginary Part')
     #x2-y axis line
     ax.axhline(y=0, color='k')
     ax.axvline(x=0, color='k')
-    ax.set_title("H^H*Y")
+    ax.set_title(title)
 
 
 # Add labels to the x and y axes
@@ -57,14 +74,9 @@ ax1  = plt.subplot2grid((1, 3), (0, 0))
 ax2  = plt.subplot2grid((1, 3), (0, 1))
 ax3  = plt.subplot2grid((1, 3), (0, 2))
 
-plot_scatter_values(ax1,rx,data)
-ax1.set_title("Y*H")
+plot_scatter_values(ax1,rx,data,"Y*H")
+plot_scatter_values(ax2,rx,hermitian,"H^H*Y")
+plot_scatter_values(ax3,rx,truth,"Truth")
 
-plot_scatter_values(ax2,rx,hermitian)
-
-ax2.set_title("H^H*Y")
-
-plot_scatter_values(ax3,rx,truth)
-ax3.set_title("Truth")
-
+#plt.savefig('./result.png')
 plt.show()
