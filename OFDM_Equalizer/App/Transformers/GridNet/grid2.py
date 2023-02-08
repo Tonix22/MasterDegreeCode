ecc74bd3c3102693cb021b30a824066ed0c762b4
Tonix22 <emiliotonix@gmail.com>
2023-02-07 21:07:44 -0600

    grid net final test

--- OFDM_Equalizer/App/Transformers/GridNet/grid2.py
+++ OFDM_Equalizer/App/Transformers/GridNet/grid2.py
@@ -1,11 +1,47 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
 
 # Define the batch size and sequence length
 batch, seqlen = 4, 48
 # Define the step size for binning
-step = 1/16
+step = 1/4
+
+# Define bins for the real and imaginary parts of the data
+binsx = torch.arange(-1, 1 + step, step)
+binsy = torch.arange(-1, 1 + step, step)
+
+
+# ----------- Color Map to scatter ---------
+cmap       = cmx.jet
+c_norm     = colors.Normalize(vmin=0, vmax=48)
+scalar_map = cmx.ScalarMappable(norm=c_norm, cmap=cmap)
+
+
+def plot_scatter_values(ax,vect,title):
+    
+    colorsMap     = [scalar_map.to_rgba(i) for i in range(vect.size)]
+    real = np.real(vect)
+    imag = np.imag(vect)
+    
+    #fig, ax2 = plt.subplots()
+    ax.scatter(real, imag,s=10, c=colorsMap,marker='*')
+    for i, txt in enumerate(range(vect.size)):
+        ax.text(real[i], imag[i],i,fontsize=10)
+
+    ax.grid(True)
+    ax.set_xticks(binsx)
+    ax.set_yticks(binsy)
+    #plot title
+    ax.set_xlabel('Real Part')
+    ax.set_ylabel('Imaginary Part')
+    #x2-y axis line
+    ax.axhline(y=0, color='k')
+    ax.axvline(x=0, color='k')
+    ax.set_title(title)
+
 
 # Generate complex valued random data and convert it to the torch complex128 data type
 data           = torch.complex(torch.rand((batch, seqlen))*2-1 ,torch.rand((batch, seqlen))*2-1).to(torch.complex128)
@@ -17,9 +53,6 @@ src_abs_factor = torch.max(torch.abs(data),dim=1, keepdim=True)[0]
 # Normalize the data by dividing it with the maximum absolute value
 data = data/src_abs_factor
 
-# Define bins for the real and imaginary parts of the data
-binsx = torch.arange(-1, 1 + step, step)
-binsy = torch.arange(-1, 1 + step, step)
 # Create a 2D bin index matrix for encoding
 binxy = torch.arange(start=4, end=4 + (len(binsx) - 1) * (len(binsy) - 1)).view(len(binsx) - 1, len(binsy) - 1)
 
@@ -37,7 +70,7 @@ for i in range(len(binsx) - 1):
 # Encode the data by selecting the corresponding bin indices from the bin index matrix
 encoded = binxy[y_indices, x_indices]
 
-"""
+
 # Create empty tensors for storing the decoded values for the real and imaginary parts of the data
 real_decoded = torch.zeros_like(data.real)
 imag_decoded = torch.zeros_like(data.imag)
@@ -56,30 +89,14 @@ for i in range(len(binsx) - 1):
 data_decoded = torch.complex(real_decoded, imag_decoded)
 
 # Denormalize the decoded data by multiplying it with the original maximum absolute value
-data_decoded = data_decoded * src_abs_factor
-"""
+#data_decoded = data_decoded * src_abs_factor
 
 
-# Extract the real and imaginary parts of the complex numbers
-real = np.real(data.numpy())
-imag = np.imag(data.numpy())
+fig =  plt.subplots()
+ax1  = plt.subplot2grid((1, 2), (0, 0))
+ax2  = plt.subplot2grid((1, 2), (0, 1))
 
-# Plot the real and imaginary parts using a scatter plot
-#plt.scatter(real, imag)
+plot_scatter_values(ax1,data[0][:10].numpy(),"Original")
+plot_scatter_values(ax2,data_decoded[0][:10].numpy(),"Decoded")
 
-# Add labels to the x and y axes
-fig, ax = plt.subplots()
-# Get the bins to plot in the graph
-ax.grid(True)
-ax.set_xticks(binsx.numpy())
-ax.set_yticks(binsy.numpy())
-#data plot
-ax.scatter(real, imag,s=10)
-#plot title
-ax.set_xlabel('Real Part')
-ax.set_ylabel('Imaginary Part')
-#x-y axis line
-ax.axhline(y=0, color='k')
-ax.axvline(x=0, color='k')
-# Show the plot
 plt.show()
