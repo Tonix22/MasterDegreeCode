9f69ba13e128839db93d607452a551ed0a1c1f9a
Tonix22 <emiliotonix@gmail.com>
2023-02-09 00:56:01 -0600

    Autoencoder predict stage

--- OFDM_Equalizer/App/Autoencoder/EncoderDecoder.py
+++ OFDM_Equalizer/App/Autoencoder/EncoderDecoder.py
@@ -11,11 +11,14 @@ import warnings
 warnings.filterwarnings("ignore", category=UserWarning) 
 main_path = os.path.dirname(os.path.abspath(__file__))+"/../../"
 sys.path.insert(0, main_path+"controllers")
+sys.path.insert(0, main_path+"tools")
 from Recieved import RX,Rx_loader
+from utils import vector_to_pandas, get_time_string
+
 
 #Hyperparameters
 BATCHSIZE  = 10
-NUM_EPOCHS = 100
+NUM_EPOCHS = 45
 QAM        = 4
 
 class AutoencoderNN(pl.LightningModule,Rx_loader):
@@ -130,22 +133,59 @@ class AutoencoderNN(pl.LightningModule,Rx_loader):
         self.log("avg_val_loss", avg_loss) #tensorboard logs
         return {'val_loss':avg_loss}
     
+    def predict_step(self, batch, batch_idx):
+        if(batch_idx < 400):
+            # training_step defines the train loop. It is independent of forward
+            chann, x = batch
+            #chann preparation
+            chann = chann.permute(0,3,1,2)
+            #torch.linalg.inv(chann[0,:,:]*abs_factor[0])
+            # ------------ Generate Y ------------
+            Y        = self.Get_Y(chann,x,conj=False,noise_activ=False)
+            
+            # ------------ Source Data ------------
+            # Normalize the first channel by dividing by max_abs
+            chann      = torch.complex(chann[:, 0, :, :],chann[:, 1, :, :])
+            abs_factor = torch.max(torch.abs(chann), dim=1, keepdim=True)[0]
+            chann      = chann/abs_factor
+            chann_abs  = torch.abs(chann)
+            
+            #normalize angle
+            chann_ang  = (torch.angle(chann) + torch.pi) / (2 * torch.pi)
+            chann_src  = torch.stack((chann_abs, chann_ang), dim=1)
+            
+            # ------------ Model Eval ------------
+            z,chann_hat = self(chann_src)
+            # ------------ Denormalize angle -----------
+            chann_hat[:,0,:,:]   = chann_hat[:,0,:,:]/abs_factor
+            chann_hat[:,1,:,:]   = chann_hat[:,1,:,:]*(2 * torch.pi)-torch.pi
+            chann_hat = torch.polar(chann_hat[:,0,:,:],chann_hat[:,1,:,:])
+            # ------------ Multiply by inverse ------------
+            x_hat = torch.einsum('bij,bi->bj', chann_hat, Y)
+            
+            self.SNR_calc(x_hat,x,norm=False) 
+            
+        return 0 
+    
     def train_dataloader(self):
         return self.train_loader
         
     def val_dataloader(self):
         return self.val_loader
     
-    def test_dataloader(self):
+    def predict_dataloader(self):
         return self.test_loader
     
 if __name__ == '__main__':
-    trainer = Trainer(accelerator='gpu',callbacks=[TQDMProgressBar(refresh_rate=10)],auto_lr_find=True, max_epochs=NUM_EPOCHS)
+    trainer = Trainer(accelerator='gpu',callbacks=[TQDMProgressBar(refresh_rate=10)],auto_lr_find=True, max_epochs=NUM_EPOCHS,
+                resume_from_checkpoint='/home/tonix/Documents/MasterDegreeCode/OFDM_Equalizer/App/Autoencoder/lightning_logs/version_5/checkpoints/epoch=44-step=54000.ckpt')
     model   = AutoencoderNN(96)
     #checkpoint_file = torch.load('/home/tonix/Documents/MasterDegreeCode/OFDM_Equalizer/App/Autoencoder/lightning_logs/Autoencoderckpt/checkpoints/epoch=999-step=1200000.ckpt')
     #print(checkpoint_file.keys())
     #model.load_state_dict(checkpoint_file['state_dict'])
-    #trainer.fit(model,ckpt_path='/home/tonix/Documents/MasterDegreeCode/OFDM_Equalizer/App/Autoencoder/lightning_logs/version_1/checkpoints/epoch=771-step=926400.ckpt')
     trainer.fit(model)
+    #name of output log file 
+    formating = "Test_(Golden_{}QAM_{})_{}".format(QAM,"PolarNet",get_time_string())
+    model.SNR_BER_TEST(trainer,formating)
     
     
